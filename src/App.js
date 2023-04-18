92f1c7d3251ced745faac64c3d7b280a329e6869
Ethan Salonga <ethansalonga28@gmail.com>
2023-04-18 17:19:34 -0400

    Add notification slice state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { useEffect } from "react"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import "./App.css"
 import Auth from "./components/Auth"
 import Layout from "./components/Layout"
+import Notification from "./components/Notification"
+import { uiActions } from "./store/ui-slice"
 
+let isFirstRender = true
 function App() {
+  const dispatch = useDispatch()
+  const notification = useSelector((state) => state.ui.notification)
   const cart = useSelector((state) => state.cart)
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn)
 
   useEffect(() => {
+    if (isFirstRender) {
+      isFirstRender = false
+      return
+    }
+
     const sendRequest = async () => {
+      // Send state as Sending request
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Sending request",
+          type: "warning",
+        })
+      )
       const res = await fetch(
         "https://redux-shopping-cart-358b9-default-rtdb.firebaseio.com/cartItems.json",
         {
@@ -19,12 +37,32 @@ function App() {
       )
 
       const data = await res.json()
+      // Send state as Success
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Sent request to database successfully",
+          type: "success",
+        })
+      )
     }
-    sendRequest()
+    sendRequest().catch((err) => {
+      // Send state as Error
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Sending request failed",
+          type: "error",
+        })
+      )
+    })
   }, [cart])
 
   return (
     <div className="App">
+      {notification && (
+        <Notification type={notification.type} message={notification.message} />
+      )}
       {!isLoggedIn && <Auth />}
       {isLoggedIn && <Layout />}
     </div>
