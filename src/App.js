e3f83f5edd22180eb41deb6f498d798c0cfa1443
ethansalonga <ethan@ethansalonga.dev>
2023-04-18 00:28:28 -0400

    Create auth state / configure redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
-import "./App.css";
-import Auth from "./components/Auth";
-import Layout from "./components/Layout";
+import React from "react"
+import { useSelector } from "react-redux"
+import "./App.css"
+import Auth from "./components/Auth"
+import Layout from "./components/Layout"
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn)
+  console.log(isLoggedIn)
+
   return (
     <div className="App">
       <Auth />
       {/* <Layout /> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
